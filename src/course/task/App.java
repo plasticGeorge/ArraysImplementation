c93e03bb036ad541b398def544bfc85c1d629e4b
plasticGeorge <georgylemko@yandex.ru>
2021-04-07 17:14:53 +0300

    Added new algorithms of sort (without mergeSort)

--- src/course/task/App.java
+++ src/course/task/App.java
@@ -4,9 +4,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        SortedArray testArr = new SortedArray(new int[]{13, 5, 3,9 , 2, 5, 6});
-        System.out.println(testArr);
-
         int[] a = new int[]{1, 3, 7, 8, 5, 6, 9, 8};
         var staticArray = new StaticArray(a);
         System.out.println(staticArray);
--- src/course/task/StaticArray.java
+++ src/course/task/StaticArray.java
@@ -18,8 +18,8 @@ public class StaticArray implements Array {
     @Override
     public boolean contains(int value) {
         // TODO: проверить, что элемент есть в массиве
-        for(int i = 0; i < array.length; i++) {
-            if (array[i] == value)
+        for (int j : array) {
+            if (j == value)
                 return true;
         }
         return false;
@@ -106,8 +106,8 @@ public class StaticArray implements Array {
         StringBuilder builder = new StringBuilder(array.length * 3 + 2);
 
         builder.append('[');
-        for (int i = 0; i < array.length; i++){
-            builder.append(array[i] + ", ");
+        for (int j : array) {
+            builder.append(j).append(", ");
         }
         builder.replace(builder.lastIndexOf(", "), builder.length(), "]");
 
@@ -133,18 +133,64 @@ public class StaticArray implements Array {
 
     private void insertionSort() {
         //TODO*: сортировка вставками (по возрастанию)
+        for(int i = 1; i < array.length; i++){
+            for(int j = i; j > 0 && array[j - 1] > array[j]; j--){
+                int temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+            }
+        }
     }
 
     private void selectionSort() {
         //TODO*: сортировка выбором (по возрастанию)
+        for(int i = 0; i < array.length; i++){
+            int min = i;
+            for(int j = i; j < array.length; j++){
+                if(array[j] < array[min])
+                    min = j;
+            }
+            if(i != min){
+                int temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+        }
     }
 
     private void mergeSort() {
         //TODO**: сортировка слиянием (по возрастанию)
     }
 
-    private void quickSort() {
+    private void quickSort(){
+        quickSort(0, array.length - 1);
+    }
+
+    private void quickSort(int start, int end) {
         //TODO**: быстрая сортировка (по возрастанию)
+        int s = start,
+            e = end,
+            pivot = array[(start + end) / 2];
+        while(s <= e){
+            while(array[s] < pivot)
+                s++;
+            while (array[e] > pivot)
+                e--;
+            if(s < e){
+                int temp = array[s];
+                array[s] = array[e];
+                array[e] = temp;
+                s++; e--;
+            }
+            else if(s == e) {
+                s++; e--;
+                break;
+            }
+        }
+        if(s < end)
+            quickSort(s, end);
+        if(e > start)
+            quickSort(start, e);
     }
 
     public boolean isAscSorted() {
